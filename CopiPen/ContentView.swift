103ec2073589da5fe61c40faf06ecd4d96622516
bannzai <kingkong999yhirose@gmail.com>
2021-08-07 22:14:51 +0900

    :memo: Replace to using CoreData

--- CopiPen/ContentView.swift
+++ CopiPen/ContentView.swift
@@ -8,54 +8,73 @@
 import SwiftUI
 import CoreData
 
-struct Content: Identifiable {
-    let id = UUID()
-    let text: String?
-    let image: UIImage?
-    init(text: String) {
-        self.text = text
-        self.image = nil
-    }
-    init(image: UIImage) {
-        self.text = nil
-        self.image = image
-    }
-}
-
-final class ContentViewModel: ObservableObject {
-    @Published var items: [Content] = []
-    
-    func update() {
-        if let image = UIPasteboard.general.image {
-            items.append(.init(image: image))
-        } else if let text = UIPasteboard.general.string {
-            items.append(.init(text: text))
-        }
-    }
-}
-
 struct ContentView: View {
-    @ObservedObject private var viewModel: ContentViewModel = .init()
-    @Environment(\.scenePhase) var scenePhase
-    
+    @Environment(\.scenePhase) private var scenePhase
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Content.timestamp, ascending: true)],
+        animation: .default)
+    private var contents: FetchedResults<Content>
+
     var body: some View {
         List {
-            ForEach(viewModel.items) { content in
-                if let text = content.text {
-                    Text("Text at \(text)")
-                }
-                if let image = content.image {
+            ForEach(contents) { content in
+                switch content.contentType {
+                case nil:
+                    EmptyView()
+                case let .text(text):
+                    Text(text)
+                case let .image(image):
                     Image(uiImage: image)
+                case let .url(url):
+                    Text(url.absoluteString)
                 }
             }
+            .onDelete(perform: deleteItems)
         }
         .onAppear(perform: {
-            print("onAppear")
-            viewModel.update()
+            deleteAll()
+            addItem()
         })
         .onChange(of: scenePhase) { scenePhase in
             if scenePhase == .active {
-                viewModel.update()
+                addItem()
+            }
+        }
+    }
+    
+    private func deleteAll() {
+        contents.forEach {
+            viewContext.delete($0)
+        }
+    }
+    
+    private func addItem() {
+        if let contentType = UIPasteboard.general.mapToContentType() {
+            withAnimation {
+                do {
+                    try Content.createAndSave(viewContext: viewContext, contentType: contentType)
+                } catch {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    let nsError = error as NSError
+                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                }
+            }
+        }
+    }
+    
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { contents[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
@@ -63,6 +82,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
