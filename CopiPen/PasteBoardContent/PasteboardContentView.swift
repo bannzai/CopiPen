934e89ad4f6864758049fba08be6a4c3045b20dd
bannzai <kingkong999yhirose@gmail.com>
2021-08-07 22:45:26 +0900

    :recycle: hiding onTapGesture details to component

--- CopiPen/PasteBoardContent/PasteboardContentView.swift
+++ CopiPen/PasteBoardContent/PasteboardContentView.swift
@@ -3,15 +3,30 @@ import SwiftUI
 struct PasteboardContentView: View {
     let content: Content
     var body: some View {
-        switch content.contentType {
-        case nil:
-            EmptyView()
-        case let .text(text):
-            PasteboardContentTextComponent(text: text)
-        case let .image(image):
-            PasteboardContentImageComponent(image: image)
-        case let .url(url):
-            PasteboardContentURLComponent(url: url)
+        Group {
+            switch content.contentType {
+            case nil:
+                EmptyView()
+            case let .text(text):
+                PasteboardContentTextComponent(text: text)
+            case let .image(image):
+                PasteboardContentImageComponent(image: image)
+            case let .url(url):
+                PasteboardContentURLComponent(url: url)
+            }
+        }
+        .onTapGesture {
+            if let contentType = content.contentType {
+                let pasteboardType = contentType.pasteboardType
+                switch contentType {
+                case .text(let text):
+                    UIPasteboard.general.setValue(text, forPasteboardType: pasteboardType)
+                case .image(let image):
+                    UIPasteboard.general.setValue(image, forPasteboardType: pasteboardType)
+                case .url(let url):
+                    UIPasteboard.general.setValue(url, forPasteboardType: pasteboardType)
+                }
+            }
         }
     }
 }
