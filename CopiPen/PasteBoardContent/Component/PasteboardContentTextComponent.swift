4748c21be8f2891a735b86e3f2957d2923461b39
bannzai <kingkong999yhirose@gmail.com>
2021-08-11 12:40:33 +0900

    :art: adjustment UI

--- CopiPen/PasteBoardContent/Component/PasteboardContentTextComponent.swift
+++ CopiPen/PasteBoardContent/Component/PasteboardContentTextComponent.swift
@@ -4,6 +4,10 @@ struct PasteboardContentTextComponent: View {
     let text: String
 
     var body: some View {
-        Text(text)
+        HStack {
+            Image(systemName: "doc.text")
+            Spacer().frame(width: 12)
+            Text(text)
+        }
     }
 }
