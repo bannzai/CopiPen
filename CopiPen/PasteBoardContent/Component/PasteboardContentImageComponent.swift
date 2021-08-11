2b05fc361d62281624c6b3b82b021e2f5a30fd1c
bannzai <kingkong999yhirose@gmail.com>
2021-08-11 09:19:26 +0900

    :sparkles: fix image rows

--- CopiPen/PasteBoardContent/Component/PasteboardContentImageComponent.swift
+++ CopiPen/PasteBoardContent/Component/PasteboardContentImageComponent.swift
@@ -6,8 +6,8 @@ struct PasteboardContentImageComponent: View {
 
     var body: some View {
         Image(uiImage: image)
-            .frame(maxHeight: 200)
-            .scaledToFill()
-            .clipped()
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(maxHeight: 100)
     }
 }
